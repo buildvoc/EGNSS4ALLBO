8bd689266129fc7524da885ea1cc853c47f340c5
Aqib <aqibhussainch@gmail.com>
2024-06-26 23:55:11 +0500

    Routes add and inital route set to login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,34 +1,68 @@
+"use client";
+
 import styles from "@/app/ui/login/login.module.css";
 import { Button } from "react-bootstrap";
+import { useRouter } from 'next/navigation';
+import { useState } from "react";
 
 const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+   const router = useRouter();
+    const handleSubmit = (event:any) => {
+        event.preventDefault();
+        router.push('/dashboard');
+      };
   return (
     <div className={styles.container}>
-       <div className={styles.login}>
-      <form action="" className={styles.login_form} >
-      <div className={styles.login_logo}></div>
-      {/* <label  title="Login:">User name:</label>
-        <input type="text" placeholder="username" />
-        <label  title="Login:">User name:</label>
-        <input type="password" placeholder="password" />
-        <button>Login</button> */}
-        <table >
+      <div className={styles.login}>
+        <form onSubmit={handleSubmit} className={styles.login_form}>
+          <div className={styles.login_logo}></div>
+          <table>
             <tr>
-            <td>
-            <label itemType="name" title="Login:">User name:</label>
-            </td>
-            <td><input className={styles.form_control} id="name" name="login" type="text" value="" /></td>
+              <td>
+                <label itemType="name" title="Login:">
+                  User name:
+                </label>
+              </td>
+              <td>
+                <input
+                  className={`form-control ${styles.form_control}`}
+                  id="username" 
+                  name="login" 
+                  type="text" 
+                  value={username} 
+                  onChange={(e) => setUsername(e.target.value)} 
+                />
+              </td>
             </tr>
             <tr>
-            <td>
-                <label itemType="name" title="Login:">User name:</label>
-            </td>
-            <td><input className={styles.form_control} id="name" name="login" type="text" value="" /></td>
+              <td>
+                <label itemType="name" title="Login:">
+                  Password:
+                </label>
+              </td>
+              <td>
+                <input
+                  className={`form-control ${styles.form_control}`}
+                  id="password" 
+                  name="password" 
+                  type="password" 
+                  value={password} 
+                  onChange={(e) => setPassword(e.target.value)} 
+                />
+              </td>
             </tr>
-        </table>
-        <button className={`btn btn-primary  ${styles.btn_primary} ${styles.btn_login}`} type="submit" value="enter">Enter</button>
-      </form>
-    </div>
+          </table>
+          <button
+            className={`btn btn-primary  ${styles.btn_primary} ${styles.btn_login}`}
+            type="submit"
+            value="enter"
+          >
+            Enter
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
