c4f2d8b47e53ec343aa29e04f14eb0acf02f7ced
Aqib <aqibhussainch@gmail.com>
2024-06-26 22:32:20 +0500

    Login page has been design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,36 @@
+import styles from "@/app/ui/login/login.module.css";
+import { Button } from "react-bootstrap";
 
 const LoginPage = () => {
-
-    return (
-        <div>
-            LoginPage
-        </div>
-    );
-}
-
+  return (
+    <div className={styles.container}>
+       <div className={styles.login}>
+      <form action="" className={styles.login_form} >
+      <div className={styles.login_logo}></div>
+      {/* <label  title="Login:">User name:</label>
+        <input type="text" placeholder="username" />
+        <label  title="Login:">User name:</label>
+        <input type="password" placeholder="password" />
+        <button>Login</button> */}
+        <table >
+            <tr>
+            <td>
+            <label itemType="name" title="Login:">User name:</label>
+            </td>
+            <td><input className={styles.form_control} id="name" name="login" type="text" value="" /></td>
+            </tr>
+            <tr>
+            <td>
+                <label itemType="name" title="Login:">User name:</label>
+            </td>
+            <td><input className={styles.form_control} id="name" name="login" type="text" value="" /></td>
+            </tr>
+        </table>
+        <button className={`btn btn-primary  ${styles.btn_primary} ${styles.btn_login}`} type="submit" value="enter">Enter</button>
+      </form>
+    </div>
+    </div>
+  );
+};
 
 export default LoginPage;
